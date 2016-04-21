0671fd0c7ee4a4d3ac247ee391eeee0871f032ab
Kevin-Tipo <tipo970@googlemail.com>
2016-04-21 12:29:26 +0200

    Methoden zum Updaten und EInlesen von Daten in die Tabellen

--- Datenerhaltungsschicht/SQL.java
+++ Datenerhaltungsschicht/SQL.java
@@ -28,15 +28,15 @@ public class SQL {
 			//Verbindung zur Datenbank
 			Connection MyConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kursverwaltung","root","");
 			
-			//Objekt zum abfragen bzw für sql anweisungen
+			//Objekt zum abfragen bzw fÃ¼r sql anweisungen
 			Statement fragMichWas = MyConnection.createStatement();
 			
 			//Abfrage
-			ResultSet test = fragMichWas.executeQuery("select * from schüler");
+			ResultSet test = fragMichWas.executeQuery("select * from schÃ¼ler");
 			
 			//Ausgabe
 			while (test.next()){
-				System.out.print(test.getString(1)+ " "); //1 steht für die Spalte kan aber auch mit namen genannt werden in ""
+				System.out.print(test.getString(1)+ " "); //1 steht fÃ¼r die Spalte kan aber auch mit namen genannt werden in ""
 				System.out.print(test.getString(2)+ " ");
 				System.out.println(test.getString(3));
 			}
@@ -49,6 +49,18 @@ public class SQL {
 		}
 		
 
+	}
+	public static void Aenderungen() {
+		String changebefehl = "UPDATE (tbl_name) SET (name = 'name') (WHERE ID = 'ID')";
+		Statement stmtDelete = con.createStatement();
+                int rowCount = stmtDelete.executeUpdate(changebefehl);
+                con.close();
+	}
+	public static void Eintragen() {
+		String eintragsbefehl ="INSERT INTO (tbl_name) VALUES (Informationen)"
+		Statement stmtDelete = con.createStatement();
+                int rowCount = stmtDelete.executeUpdate(eintragsbefehl);
+                con.close();
 	}
 
 }
