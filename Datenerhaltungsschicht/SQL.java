73de8a49add2f5bafac4063e7c9843a1d300adfb
Zimmerjo <softwear.license@googlemail.com>
2016-06-08 17:30:03 +0200

    server

--- Datenerhaltungsschicht/SQL.java
+++ Datenerhaltungsschicht/SQL.java
@@ -3,12 +3,14 @@ package Datenerhaltungsschicht;
 import java.sql.*;
 public class SQL {
 	
+	private static String[] server = {"jdbc:mysql://pazim.de:3306/kursverwaltung","db12612539-skurs","EhgYwZp2"};
+	
 	public SQL(){
 		
 	}
 		
 	public ResultSet abfrage(String befehl) throws SQLException{
-		Connection MyConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kursverwaltung","root","");
+		Connection MyConnection = DriverManager.getConnection(server[0],server[1],server[2]);
 		Statement fragMichWas = MyConnection.createStatement();
 		ResultSet ergebnis = fragMichWas.executeQuery(befehl);
 		return ergebnis;
@@ -17,7 +19,7 @@ public class SQL {
 	
 	
 	public void Verbindung() throws SQLException{
-		Connection MyConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kursverwaltung","root","");
+		Connection MyConnection = DriverManager.getConnection(server[0],server[1],server[2]);
 	}
 	
 	
@@ -26,17 +28,17 @@ public class SQL {
 		try {
 			
 			//Verbindung zur Datenbank
-			Connection MyConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kursverwaltung","root","");
+			Connection MyConnection = DriverManager.getConnection(server[0],server[1],server[2]);
 			
-			//Objekt zum abfragen bzw für sql anweisungen
+			//Objekt zum abfragen bzw fÃ¼r sql anweisungen
 			Statement fragMichWas = MyConnection.createStatement();
 			
 			//Abfrage
-			ResultSet test = fragMichWas.executeQuery("select * from schüler");
+			ResultSet test = fragMichWas.executeQuery("select * from schÃ¼ler");
 			
 			//Ausgabe
 			while (test.next()){
-				System.out.print(test.getString(1)+ " "); //1 steht für die Spalte kan aber auch mit namen genannt werden in ""
+				System.out.print(test.getString(1)+ " "); //1 steht fÃ¼r die Spalte kan aber auch mit namen genannt werden in ""
 				System.out.print(test.getString(2)+ " ");
 				System.out.println(test.getString(3));
 			}
@@ -53,9 +55,9 @@ public static void SQL_abfragen(String abfrage) {
 		
 		try {			
 			//Verbindung zur Datenbank
-			Connection MyConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kursverwaltung","root","");
+			Connection MyConnection = DriverManager.getConnection(server[0],server[1],server[2]);
 			
-			//Objekt zum abfragen bzw für sql anweisungen
+			//Objekt zum abfragen bzw fÃ¼r sql anweisungen
 			Statement fragMichWas = MyConnection.createStatement();
 			
 			//Abfrage
@@ -70,7 +72,7 @@ public static void SQL_abfragen(String abfrage) {
 				
 			
 //			while (ergebnis_res.next()){
-//				System.out.print(ergebnis_res.getString(1)+ " "); //1 steht für die Spalte kan aber auch mit namen genannt werden in ""
+//				System.out.print(ergebnis_res.getString(1)+ " "); //1 steht fÃ¼r die Spalte kan aber auch mit namen genannt werden in ""
 //				System.out.print(ergebnis_res.getString(2)+ " ");
 //				System.out.println(ergebnis_res.getString(3));
 //			}
